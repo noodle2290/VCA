4f9aa52ecd1f75715fd500b131853b4a4a0e23f1
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-06-05 01:53:27 +0900

    　MenuAdapterを作る前

--- app/src/main/java/app/murakami/takuro/voicecookingassistant/RecipeData.kt
+++ app/src/main/java/app/murakami/takuro/voicecookingassistant/RecipeData.kt
@@ -1,11 +1,14 @@
 package app.murakami.takuro.voicecookingassistant
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.time.LocalDateTime
+import java.util.*
 
 
 //Realmに保存するクラス
 open class RecipeData (
+    @PrimaryKey open var id: String = UUID.randomUUID().toString(),
     open var menu:String = "",
     open var ingredients:String = "",
     open var method:String = ""
